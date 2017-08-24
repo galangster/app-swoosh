c8aef079c6854b18733e3c69b8795a80ca59602d
Nicholas Galang <nicholasgalang@gmail.com>
2017-08-23 17:49:52 -1000

    IBActions and Data Modeling

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Nicholas Galang on 8/23/17.
+//  Copyright © 2017 Nick Galang. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
